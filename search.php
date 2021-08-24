f539d292e29e5aec01baa4384fbf18b5c3ca870d
Aniket Pathak <aniketpathak028@gmail.com>
2021-08-24 22:40:01 +0530

    add backend logic for search page

---
+++ search.php
@@ -0,0 +1,53 @@
+<?php
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$dbname = "aeropal_db";
+$con = new mysqli($localhost, $username, $password, $dbname);
+if( $con->connect_error){
+    die('Error: ' . $con->connect_error);
+}
+$sql = "SELECT * FROM airport_maps";
+if( isset($_GET['airport']) ){
+    $airport = mysqli_real_escape_string($con, htmlspecialchars($_GET['airport']));
+    $airline = mysqli_real_escape_string($con, htmlspecialchars($_GET['airline']));
+    $date = mysqli_real_escape_string($con, htmlspecialchars($_GET['date']));
+    $sql = "SELECT * FROM airport_maps WHERE airport= '$airport' AND  airline= '$airline' AND dt= '$date' ";
+}
+$result = $con->query($sql);
+?>
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700"
+    />
+    <link rel="stylesheet" href="search.css">
+    <title>Search-Results</title>
+  </head>
+  <body>
+    <div class="result">
+    <h1>Search Result:</h1>
+
+        <?php
+        while($row = $result->fetch_assoc()){
+            ?>
+
+            <img src= "<?php echo $row['map'];?>">
+
+            <?php
+        }
+        ?>
+  </div>
+  <marquee behavior=scroll direction="right" scrollamount="5" scrolldelay="10">
+  <h2>Happy Journey! Have a safe flight! </h2>
+      </marquee>
+  </body>
+  </html>
+
+  
