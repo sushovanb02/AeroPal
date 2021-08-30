7ae2f605508adb024ee4229320b1d3ca40ae65d1
sushovanb02 <sushovanb02@gmail.com>
2021-08-30 12:17:47 +0530

    search results added

--- search.php
+++ search.php
@@ -8,12 +8,12 @@ if( $con->connect_error){
     die('Error: ' . $con->connect_error);
 }
 $sql = "SELECT * FROM airport_maps";
-if( isset($_GET['airport']) ){
+if( isset($_GET['airport'])){
     $airport = mysqli_real_escape_string($con, htmlspecialchars($_GET['airport']));
     $date = mysqli_real_escape_string($con, htmlspecialchars($_GET['date']));
     $flight_id= mysqli_real_escape_string($con, htmlspecialchars($_GET['flight_id']));
-    $sql = "SELECT * FROM airport_maps WHERE airport= '$airport' AND dt= '$date' AND 
-    flight_id='$flight_id'";
+    $sql = "SELECT * FROM airport_maps WHERE airport= '$airport' AND dt= '$date' AND
+    flight_id ='$flight_id'";
 }
 $result = $con->query($sql);
 ?>
@@ -23,32 +23,34 @@ $result = $con->query($sql);
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="search.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700"
-    />
-    <link rel="stylesheet" href="search.css">
     <title>Search-Results</title>
   </head>
   <body>
     <div class="result">
     <h1>Search Result:</h1>
-
+    
         <?php
-        while($row = $result->fetch_assoc()){
-            ?>
+            
+            if(mysqli_num_rows($result) <= 0)
+            {
+                echo "<h3>Sorry, No results found! :( Kindly check your info!!</h3>";
+            } 
+            else 
+            {
+                while($row = $result->fetch_assoc()){
+                ?>
+                    <img src= "<?php echo $row['map'];?>">
 
-            <img src= "<?php echo $row['map'];?>">
+                <?php
+                }
+                ?>
 
-            <?php
-        }
-        ?>
+                <h2> Happy Journey! Have a safe flight!</h2>
+                <?php
+            } ?>
+  
   </div>
-  <marquee behavior=scroll direction="right" scrollamount="5" scrolldelay="10">
-  <h2>Happy Journey! Have a safe flight! </h2>
-      </marquee>
   </body>
   </html>
-
-  
